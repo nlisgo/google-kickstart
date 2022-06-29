1f1d543a9eb352671d35e10a157d31c70985bd90
nlisgo <nathan@aeopus.com>
2022-06-29 19:27:17 +0100

    Refactor tests

--- 2013/test/practice/captain-hammer.test.js
+++ 2013/test/practice/captain-hammer.test.js
@@ -4,35 +4,40 @@ describe('splitInput', () => {
     test('sample.in', () => {
         expect(
             captainHammer.splitInput(`3
-one
-two
-three`.split(/\r?\n/))
+98 980
+98 490
+299 1234`.split(/\r?\n/))
         )
         .toStrictEqual(
             [
-                'one',
-                'two',
-                'three',
+                [
+                    98,
+                    980,
+                ],
+                [
+                    98,
+                    490,
+                ],
+                [
+                    299,
+                    1234,
+                ],
             ]
         );
     });
 });
 
 describe('solve', () => {
-    test('one', () => {
-        expect(captainHammer.solve('one')).toBe('one');
+    test('[98, 980]', () => {
+        expect(captainHammer.solve([98, 980])).toBe('45.0000000');
     });
 
-    test('two', () => {
-        expect(captainHammer.solve('two')).toBe('two');
+    test('[98, 490]', () => {
+        expect(captainHammer.solve([98, 490])).toBe('15.0000000');
     });
 
-    test('three', () => {
-        expect(captainHammer.solve('three')).toBe('three');
-    });
-
-    test('four', () => {
-        expect(captainHammer.solve('four')).toBe('four');
+    test('[299, 1234]', () => {
+        expect(captainHammer.solve([299, 1234])).toBe('3.8870928');
     });
 });
 
@@ -40,15 +45,15 @@ describe('solveInputs', () => {
     test('sample.in', () => {
         expect(
             captainHammer.solveInputs(`3
-one
-two
-three`.split(/\r?\n/))
+98 980
+98 490
+299 1234`.split(/\r?\n/))
         )
         .toStrictEqual(
             [
-                'Case #1: one',
-                'Case #2: two',
-                'Case #3: three',
+                'Case #1: 45.0000000',
+                'Case #2: 45.0000000',
+                'Case #3: 3.8870928',
             ]
         );
     });
